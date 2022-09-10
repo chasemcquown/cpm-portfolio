5ea640d18878e3adab825720cb437ce26c3deb74
chasem <cpmcquown@gmail.com>
2022-09-10 15:18:26 -0700

    adds icon hover effects

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,24 @@
-function btnHTML() {
-    document.querySelector('.primary-btn').innerHTML = "what I've been working on   " + '<i class="fa-solid fa-angle-down"></i>'
-    document.querySelector('#btn-1').innerHTML = 'view project  ' + '<i class="fa-solid fa-angle-right"></i>'
-    document.querySelector('#btn-2').innerHTML = 'view project  ' + '<i class="fa-solid fa-angle-right"></i>'
-    document.querySelector('#btn-3').innerHTML = 'view project  ' + '<i class="fa-solid fa-angle-right"></i>'
-    document.querySelector('#btn-4').innerHTML = 'view project  ' + '<i class="fa-solid fa-angle-right"></i>'
+// if not hovering, revert to original icon
+function mouseOutIcon() {
+
+    if(event.target.id === 'hero-btn') {
+        document.querySelector('.primary-btn').innerHTML = "what I've been working on   " + '<i class="fa-solid fa-angle-down"></i>'
+    }
+
+    if(event.target.id === 'btn-1') {
+        document.querySelector("#btn-1").innerHTML = 'view project  ' + '<i class="fa-solid fa-angle-right"></i>'
+    } else if (event.target.id === 'btn-2') {
+        document.querySelector("#btn-2").innerHTML = 'view project  ' + '<i class="fa-solid fa-angle-right"></i>'
+    } else if (event.target.id === 'btn-3') {
+        document.querySelector("#btn-3").innerHTML = 'view project  ' + '<i class="fa-solid fa-angle-right"></i>'
+    } else if (event.target.id === 'btn-4') {
+        document.querySelector("#btn-4").innerHTML = 'view project  ' + '<i class="fa-solid fa-angle-right"></i>'
+    }
+
 }
 
-function changeIcon() {
+// if hovering, change icon on hover
+function hoverIcon() {
 
     if(event.target.id === 'hero-btn') {
         document.querySelector('.primary-btn').innerHTML = "what I've been working on   " + '<i class="fa-solid fa-angles-down"></i>'
@@ -24,7 +36,8 @@ function changeIcon() {
 
 }
 
+// listen for hover and mouseout and then change icon based on condition
+document.addEventListener('mouseout', mouseOutIcon)
+document.addEventListener('mouseover', hoverIcon)
 
-document.addEventListener('mouseover', changeIcon)
-btnHTML()
 
